d52c913fdc710d9ccf2701485271d83cb48c9f82
martin.fischer <martin.fischer@kontron.com>
2023-07-14 15:49:09 +0200

    Finally API works

--- src/main/java/net/fiscma/wazenhof/warzenhofbackend/model/Dish.java
+++ src/main/java/net/fiscma/wazenhof/warzenhofbackend/model/Dish.java
@@ -1,14 +1,17 @@
 package net.fiscma.wazenhof.warzenhofbackend.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import org.springframework.data.domain.Pageable;
 
 import java.io.Serializable;
 
 @Entity
 @Table(name = "DISH")
+@Data
 public class Dish implements Serializable{
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Basic
--- src/main/java/net/fiscma/wazenhof/warzenhofbackend/service/impl/BaseServiceImpl.java
+++ src/main/java/net/fiscma/wazenhof/warzenhofbackend/service/impl/BaseServiceImpl.java
@@ -1,19 +1,23 @@
 package net.fiscma.wazenhof.warzenhofbackend.service.impl;
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import net.fiscma.wazenhof.warzenhofbackend.repository.BaseRepository;
 import net.fiscma.wazenhof.warzenhofbackend.service.BaseService;
 import org.hibernate.annotations.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 
 @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
 @Service
+@Transactional
 public abstract class BaseServiceImpl<T> implements BaseService<T> {
     @Autowired
     private BaseRepository<T, Long> repository;
@@ -24,8 +28,12 @@ public abstract class BaseServiceImpl<T> implements BaseService<T> {
     }
 
     @Override
-    public T get(Long id) {
-        return repository.findById(id).orElseThrow(NoSuchElementException::new);
+    public T get(Long id)  {
+        return repository.findById(id).orElseThrow(
+          () -> {
+              throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+          }
+        );
     }
 
     @Override
