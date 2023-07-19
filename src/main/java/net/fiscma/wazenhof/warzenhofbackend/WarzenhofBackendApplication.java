792f7fc806914aba3e05af9308e1d98434e43790
horn1347 <horn1347@users.noreply.github.com>
2023-07-19 18:15:06 +0200

    Not too promising at first glance

--- src/main/java/net/fiscma/wazenhof/warzenhofbackend/WarzenhofBackendApplication.java
+++ src/main/java/net/fiscma/wazenhof/warzenhofbackend/WarzenhofBackendApplication.java
@@ -1,13 +1,31 @@
 package net.fiscma.wazenhof.warzenhofbackend;
 
+import jakarta.servlet.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.faces.webapp.FacesServlet;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class WarzenhofBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WarzenhofBackendApplication.class, args);
+	}
+
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean(ServletContext servletContext) {
+		servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+
+		//registration
+		ServletRegistrationBean srb = new ServletRegistrationBean();
+		srb.setServlet(new FacesServlet());
+		srb.setUrlMappings(Arrays.asList("*.xhtml"));
+		srb.setLoadOnStartup(1);
+		return srb;
 	}
 
 }
--- src/main/java/net/fiscma/wazenhof/warzenhofbackend/rest/controller/impl/DishRestControllerImpl.java
+++ src/main/java/net/fiscma/wazenhof/warzenhofbackend/rest/controller/impl/DishRestControllerImpl.java
@@ -3,16 +3,13 @@ package net.fiscma.wazenhof.warzenhofbackend.rest.controller.impl;
 import net.fiscma.wazenhof.warzenhofbackend.model.Dish;
 import net.fiscma.wazenhof.warzenhofbackend.rest.controller.BaseRestController;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/dish")
+@RequestMapping(value = "/api/dish")
 public class DishRestControllerImpl extends BaseRestControllerImpl<Dish> implements BaseRestController<Dish> {
     @PostMapping(path = "/create/{name}")
-    public ResponseEntity<Dish> save(@PathVariable String name) {
+    public ResponseEntity<Dish> create(@PathVariable String name) {
         Dish dish = new Dish();
         dish.setName(name);
         return ResponseEntity.ok(service.create(dish));
